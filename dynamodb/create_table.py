00f48467ff64ed113ca468a2d00d33df8f2d04ca
ZuemITInc <zemetraining@zumeit.com>
2020-04-30 23:39:29 +0530

    It creates the DynamoDB table

---
+++ dynamodb/create_table.py
@@ -0,0 +1,39 @@
+import boto3
+
+client = boto3.resource('dynamodb')
+
+table = client.create_table(
+    TableName='Movies',
+    KeySchema=[
+        {
+            'AttributeName': 'year',
+            'KeyType': 'HASH'  #Partition key
+        },
+        {
+            'AttributeName': 'title',
+            'KeyType': 'RANGE'  #Sort key
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'year',
+            'AttributeType': 'N'
+        },
+        {
+            'AttributeName': 'title',
+            'AttributeType': 'S'
+        },
+
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 10,
+        'WriteCapacityUnits': 10
+    },
+    Tags=[
+        {
+            'Key': 'name',
+            'Value': 'movies'
+        },
+    ]
+)
+print('Table created successfully..')
