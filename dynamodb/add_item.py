649ace0a0825ddfb134068efd50d259bc24662aa
ZuemITInc <zemetraining@zumeit.com>
2020-05-01 01:11:59 +0530

    It adds an item to the DynamoDb table.

--- dynamodb/add_item.py
+++ dynamodb/add_item.py
@@ -2,6 +2,7 @@ import boto3
 import json
 import decimal
 
+
 # Helper class to convert a DynamoDB item to JSON.
 class DecimalEncoder(json.JSONEncoder):
     def default(self, o):
@@ -34,4 +35,45 @@ def add_item():
     return
 
 print(add_item())
+
+
+
+#dynamodb= boto3.resource('dynamodb')
+#table= dynamodb.Table('Movies')
+#
+#response= table.put_item(
+#    Item={
+#        "year" : 2018,
+#        "title" : "KGF chapter I!",
+#        "info" : {
+#            "directors" : [
+#                "Prashanth Neel"
+#            ],
+#            "producer" : [
+#              "Vijay Kiragandur"
+#            ],
+#            "release_date" : "2018-December-21",
+#            "rating" : 8,
+#            "genres" : [
+#                "Action",
+#                "Adventure"
+#            ],
+#            "image_url" : "https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2Fc%2Fcc%2FK.G.F_Chapter_1_poster.jpg&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FK.G.F%3A_Chapter_1&tbnid=fEJWBaHUgHoK6M&vet=12ahUKEwjNhISzrpDpAhXg-jgGHckECk0QMygBegUIARDnAQ..i&docid=1vfO3zLZJXMOgM&w=223&h=300&q=k.g.f&ved=2ahUKEwjNhISzrpDpAhXg-jgGHckECk0QMygBegUIARDnAQ",
+#            "plot" : "A Majority of the film is set in Kolar Gold Fields and was filmed on location.",
+#            "rank" : 10,
+#            "running_time" : "2-hour-36-minutes",
+#            "actors" : [
+#                "Yash",
+#                "Srinidi Shetty",
+#                "Ramachandra Raju"
+#           ]
+#        }
+#    }
+#)
+#
+#print("Putitem successfull.....")
+#print(response)
+#
+
+
 
